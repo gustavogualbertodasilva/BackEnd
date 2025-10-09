c216b600e278e8d6be5eb090dea328589d4443ae
SeuNomeDeUsuario <SeuEmail@exemplo.com>
2025-10-09 12:14:36 -0300

    Comit 09/10/25

---
+++ C#/Exercicios/Tuplas/Tuplas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        (string a, int b, bool c) pessoa = CadastrarPessoa("joao", 17);
+        System.Console.WriteLine($"Nome: {pessoa.a}, Idade: {pessoa.b}, Maior? = {pessoa.c} ");
+    }
+
+    public static (string nome,int idade, bool maiorDeIdade) CadastrarPessoa(string a, int b)
+    {
+        bool c;
+        if (b >= 18)
+        {
+            c = true;
+        }
+        else
+        {
+            c = false;
+        }
+        return (a, b, c);
+    }
+}
